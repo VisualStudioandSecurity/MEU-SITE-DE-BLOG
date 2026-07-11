1b7b86f6306c464d1c3191a27534e60f22193e21
Kovaliosky <abadioguilherme76@gmail.com>
2026-07-10 22:15:30 -0300

    Create collapsible.jsx

---
+++ collapsible.jsx
@@ -0,0 +1,14 @@
+apps/web/src/components/ui/collapsible.jsx
+
+"use client"
+
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+const Collapsible = CollapsiblePrimitive.Root
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
+
